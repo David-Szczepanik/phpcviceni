ef2211b9d57a4f676310d6a275c1146632a9a5a3
David Szczepanik <davidszczepanik@mail.com>
2024-03-13 12:21:23 +0100

    db

--- add.php
+++ add.php
@@ -6,6 +6,7 @@ $dbname = "library";
 
 
 
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
--- index.php
+++ index.php
@@ -1,35 +1,3 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "admin";
-$dbname = "library";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $isbn = $_POST["isbn"];
-  $firstname = $_POST["firstname"];
-  $lastname = $_POST["lastname"];
-  $title = $_POST["title"];
-  $description = $_POST["description"];
-
-  $sql = "INSERT INTO Books (isbn, firstname, lastname, title, description)
-  VALUES ('$isbn', '$firstname', '$lastname', '$title', '$description')";
-
-  if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-}
-
-$conn->close();
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,22 +7,40 @@ $conn->close();
 </head>
 <body>
 
-<!-- Your PHP and HTML code here -->
-
 <div class="topnav">
-  <a class="active" href="add.php">Add Book</a>
-  <a href="search.php">Search Book</a>
-  <a href="index.php">Book List</a>
+    <a class="active" href="add.php">Add Book</a>
+    <a href="search.php">Search Book</a>
+    <a href="index.php">Book List</a>
 </div>
 
-<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-  ISBN: <input type="text" name="isbn">
-  First name: <input type="text" name="firstname">
-  Last name: <input type="text" name="lastname">
-  Title: <input type="text" name="title">
-  Description: <textarea name="description" rows="5" cols="40"></textarea>
-  <input type="submit">
-</form>
 
-</body>
-</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "library";
+
+// Vytvoření spojení
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kontrola spojení
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, isbn, firstname, lastname, title, description FROM Books";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // výpis dat každého řádku
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - ISBN: " . $row["isbn"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. " - Title: " . $row["title"]. " - Description: " . $row["description"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+
+
